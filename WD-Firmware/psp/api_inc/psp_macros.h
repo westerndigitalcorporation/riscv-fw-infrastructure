fc4f3002ea2e9e1e28f8f01cafcf1c9bd0212d43
ronen.haen@wdc.com <ronen.haen@wdc.com>
2020-01-28 08:59:57 +0200

    CR changes: rename _BUILTIN_EXPECT to M_PSP_BUILTIN_EXPECT tested by adding lock/unlock calls to the baremetal demo

--- WD-Firmware/psp/api_inc/psp_macros.h
+++ WD-Firmware/psp/api_inc/psp_macros.h
@@ -116,7 +116,7 @@
    comparison value. If it is equal to 1 (true), the condition
    is likely to be true, in other case condition is likely to be false.
    this provides us a way to take rare cases out of the critical execution path */
-#define _BUILTIN_EXPECT(condition, expected)  __builtin_expect(condition, expected)
+#define M_PSP_BUILTIN_EXPECT(condition, expected)  __builtin_expect(condition, expected)
 /*******************************************************/
 
 
