7ee30daac9834471d9177f6f2874a83df46ac9ec
ronen.haen@wdc.com <ronen.haen@wdc.com>
2020-02-24 13:52:51 +0200

    align freerto demo to new fpga

--- WD-Firmware/demo/build/demos/demo_freertos.py
+++ WD-Firmware/demo/build/demos/demo_freertos.py
@@ -27,9 +27,6 @@ class demo(object):
 
     self.public_defs = [
         'D_USE_RTOSAL',
-        'D_MTIME_ADDRESS=0x0200BFF8',
-        'D_MTIMECMP_ADDRESS=0x02004000',
-        'D_CLOCK_RATE=32768',
         'D_TICK_TIME_MS=4',
         'D_ISR_STACK_SIZE=400',
         'D_USE_FREERTOS'
