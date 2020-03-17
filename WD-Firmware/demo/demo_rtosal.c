e5489936be1a5ca3ad9fe8f4eb2f8f25577c3e5e
Alexander Dvoskin <Alexander.Dvoskin@wdc.com>
2020-03-17 10:23:15 -0400

    Moving ext int handler registartion before RTOS start

--- WD-Firmware/demo/demo_rtosal.c
+++ WD-Firmware/demo/demo_rtosal.c
@@ -153,12 +153,12 @@ static s08_t cQueueBuffer[D_MAIN_QUEUE_LENGTH * sizeof(u32_t)];
  */
 void demoStart(void)
 {
-	rtosalStart(demoRtosalCreateTasks);
-
 #if defined(D_NEXYS_A7)
     /* register external interrupt handler */
     pspRegisterInterruptHandler(pspExternalIntHandlerIsr, E_MACHINE_EXTERNAL_CAUSE);
 #endif
+
+	rtosalStart(demoRtosalCreateTasks);
 }
 
 /**
