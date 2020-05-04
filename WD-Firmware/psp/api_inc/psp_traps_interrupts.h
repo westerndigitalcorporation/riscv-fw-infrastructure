ae8b8ba3b0d605867bcb8951192da3f87ab95800
nati rapaport <nati.rapaport@wdc.com>
2020-05-04 17:31:03 +0300

    Interrupt-Registration - split to common & eh1 parts

--- WD-Firmware/psp/api_inc/psp_traps_interrupts.h
+++ WD-Firmware/psp/api_inc/psp_traps_interrupts.h
@@ -35,47 +35,6 @@
 /**
 * types
 */
-
-typedef enum pspInterruptCause
-{
-   E_USER_SOFTWARE_CAUSE             = 0,
-   E_SUPERVISOR_SOFTWARE_CAUSE       = 1,
-   E_RESERVED_SOFTWARE_CAUSE         = 2,
-   E_MACHINE_SOFTWARE_CAUSE          = 3,
-   E_USER_TIMER_CAUSE                = 4,
-   E_SUPERVISOR_TIMER_CAUSE          = 5,
-   E_RESERVED_TIMER_CAUSE            = 6,
-   E_MACHINE_TIMER_CAUSE             = 7,
-   E_USER_EXTERNAL_CAUSE             = 8,
-   E_SUPERVISOR_EXTERNAL_CAUSE       = 9,
-   E_RESERVED_EXTERNAL_CAUSE         = 10,
-   E_MACHINE_EXTERNAL_CAUSE          = 11,
-   E_LAST_COMMON_CAUSE
-} ePspInterruptCause_t;
-
-
-/* Exceptions */
-typedef enum pspExceptionCause
-{
-   E_EXC_INSTRUCTION_ADDRESS_MISALIGNED           = 0,
-   E_EXC_INSTRUCTION_ACCESS_FAULT                 = 1,
-   E_EXC_ILLEGAL_INSTRUCTION                      = 2,
-   E_EXC_BREAKPOINT                               = 3,
-   E_EXC_LOAD_EXC_ADDRESS_MISALIGNED              = 4,
-   E_EXC_LOAD_EXC_ACCESS_FAULT                    = 5,
-   E_EXC_STORE_AMO_ADDRESS_MISALIGNED             = 6,
-   E_EXC_STORE_AMO_ACCESS_FAULT                   = 7,
-   E_EXC_ENVIRONMENT_CALL_FROM_UMODE              = 8,
-   E_EXC_ENVIRONMENT_CALL_FROM_SMODE              = 9,
-   E_EXC_RESERVED                                 = 10,
-   E_EXC_ENVIRONMENT_CALL_FROM_MMODE              = 11,
-   E_EXC_INSTRUCTION_PAGE_FAULT                   = 12,
-   E_EXC_LOAD_EXC_PAGE_FAULT                      = 13,
-   E_EXC_RESERVEE_EXC_FOR_FUTURE_STANDARE_EXC_USE = 14,
-   E_EXC_STORE_AMO_PAGE_FAULT                     = 15,
-   E_EXC_LAST_COMMON
-} ePspExceptionCause_t;
-
 typedef enum pspExternIntHandlerPrivilege
 {
    E_EXT_USER_INT_HNDLR       = E_USER_EXTERNAL_CAUSE,
@@ -84,9 +43,6 @@ typedef enum pspExternIntHandlerPrivilege
    E_EXT_INT_HNDLR_LAST
 } ePspExternIntHandlerPrivilege_t;
 
-
-/* interrupt handler definition */
-typedef void (*pspInterruptHandler_t)(void);
 
 /**
 * definitions
@@ -140,38 +96,15 @@ typedef void (*pspInterruptHandler_t)(void);
 */
 void pspInterruptsSetVectorTableAddress(void* pVectTable);
 
-
-/**
-* @brief - The function installs an interrupt service routine per risc-v cause
-*
-* input parameter  fptrInterruptHandler     - function pointer to the interrupt service routine
-* input parameter  interruptCause           - interrupt source
-* return u32_t                              - previously registered ISR
-*/
-pspInterruptHandler_t pspRegisterInterruptHandler(pspInterruptHandler_t fptrInterruptHandler, u32_t uiInterruptCause);
-
-
-/**
-* @brief - The function installs an exception handler per exception cause
-*
-* input parameter fptrInterruptHandler     - function pointer to the exception handler
-* input parameter exceptionCause           - exception cause
-* return u32_t                             - previously registered ISR
-*/
-pspInterruptHandler_t pspRegisterExceptionHandler(pspInterruptHandler_t fptrInterruptHandler, u32_t uiExceptionCause);
-
-
 /**
 * @brief - default empty interrupt handler
 */
 void pspDefaultEmptyIntHandler_isr(void);
 
-
 /**
 * @brief - Function that called upon unregistered Trap handler
 */
 void pspTrapUnhandled(void);
-
 
 /**
 * @brief - Disable interrupts and return the current interrupt state in each one of the privileged levels
@@ -179,7 +112,6 @@ void pspTrapUnhandled(void);
 * output parameter - Current (== before the 'disable') interrupts state in each one of the privileged levels (read from mstatus CSR)
 */
 void pspInterruptsDisable(u32_t  *pOutPrevIntState);
-
 
 /**
 * @brief - Restore the interrupts state in each one of the privileged levels.
@@ -189,12 +121,10 @@ void pspInterruptsDisable(u32_t  *pOutPrevIntState);
 */
 void pspInterruptsRestore(u32_t uiPrevIntState);
 
-
 /**
 * @brief - Enable interrupts (in all privilege levels) regardless their previous state
 */
 void pspInterruptsEnable(void);
-
 
 
 /******************************************************************
@@ -215,8 +145,6 @@ void pspInterruptsEnable(void);
 */
 void pspDisableInterruptNumberMachineLevel(u32_t uiInterruptNumber);
 
-
-
 /*****************************************************************
 * @brief - Enable specified interrupt when called in MACHINE-LEVEL
 *                                                    *************
@@ -235,8 +163,6 @@ void pspDisableInterruptNumberMachineLevel(u32_t uiInterruptNumber);
 */
 void pspEnableInterruptNumberMachineLevel(u32_t uiInterruptNumber);
 
-
-
 /***************************************************************
 * @brief - Disable specified interrupt when called in USER-LEVEL
 *                                                     **********
@@ -250,8 +176,6 @@ void pspEnableInterruptNumberMachineLevel(u32_t uiInterruptNumber);
 */
 void pspDisableInterruptNumberUserLevel(u32_t uiInterruptNumber);
 
-
-
 /***************************************************************
 * @brief - Enable specified interrupt when called in USER-LEVEL
 *                                                     **********
@@ -264,8 +188,6 @@ void pspDisableInterruptNumberUserLevel(u32_t uiInterruptNumber);
 * input parameter - Interrupt number to enable
 */
 void pspEnableInterruptNumberUserLevel(u32_t uiInterruptNumber);
-
-
 
 
 #endif /* __PSP_TRAPS_INTERRUPTS_H__ */
