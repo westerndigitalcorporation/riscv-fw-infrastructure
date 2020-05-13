8f4afe8ed5e5287dbfb2b7d841542034e1813019
nati rapaport <nati.rapaport@wdc.com>
2020-05-13 19:32:11 +0300

    Add PSP version

---
+++ WD-Firmware/psp/psp_version.c
@@ -0,0 +1,69 @@
+/*
+* SPDX-License-Identifier: Apache-2.0
+* Copyright 2019 Western Digital Corporation or its affiliates.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http:*www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+* @file   psp_version.c
+* @author Nati Rapaport
+* @date   13.05.2020
+* @brief  The file maintains PSP version and suply it upon demand
+*/
+
+
+/**
+* include files
+*/
+#include "psp_api.h"
+
+/**
+* definitions
+*/
+#define PSP_VERSION_MAJOR  1
+#define PSP_VERSION_MINOR  0
+
+/**
+* macros
+*/
+
+/**
+* types
+*/
+
+/**
+* local prototypes
+*/
+
+/**
+* external prototypes
+*/
+
+/**
+* global variables
+*/
+
+/**
+* functions
+*/
+/**
+* @brief - The function return PSP version 
+*
+* @param input: pointer to PSP version structure
+*
+*/
+void pspGetVersion(pspVersion_t *pPspVersion)
+{
+  pPspVersion->usMajor = (u16_t)PSP_VERSION_MAJOR;
+  pPspVersion->usMinor = (u16_t)PSP_VERSION_MINOR;
+}
