44f10b10939affc444ffd421eacd0bb5f9940115
Ronen Haen <Ronen.Haen@wdc.com>
2020-08-02 10:35:51 +0000

    Merge pull request #97 in CTORISCVFWINFRA/riscv-fw-infrastructure from b-comrv-rtos to master
    Squashed commit of the following:
    
    commit 7b5c4877ec840fa14233bcae2bde311c99a62809
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Thu Jul 30 20:54:38 2020 +0300
    
        changes:
        use __riscv_bitmanip define in comrv
        new whisper version - fix registers and CSRs exposed to the end user
    
    commit 9675ce3d03029058a70f1776bb45398ed65ed047
    Merge: 371d28f 735067e
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Thu Jul 30 12:24:21 2020 +0300
    
        Merge remote-tracking branch 'origin/master' into loc/b-comrv-rtos
    
    commit 371d28fe9ce043f3c5e614856cb477b00f0d00d3
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Thu Jul 30 11:41:46 2020 +0300
    
        align to llvm fix:
        I've reported an issue with llvm (07-08-2019): 'riscv inline assembly input operand failure'; this issue was fixed and our llvm has this fix, so I modify the comrv code accordingly. Link to the issue https://bugs.llvm.org/show_bug.cgi?id=42912
    
    commit e1f0cc4a57118b7c5975985cf7a4bc0b64bf0950
    Merge: b22329e 09c771a
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Thu Jul 30 10:44:40 2020 +0300
    
        Merge remote-tracking branch 'origin/master' into loc/b-comrv-rtos
    
    commit b22329eb0e78adf6e084628881b1078aaaba84e4
    Merge: 62b3962 27b7600
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Tue Jul 28 16:57:48 2020 +0300
    
        merge
    
    commit 62b3962936db326de6fcd4c7e91421ec1cb1b30d
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Tue Jul 28 16:55:37 2020 +0300
    
        new whisper version:
        whisper version with a fix related to sw interrupt
        comrv - empty macro for triggering sw interrupt
        comrv - add missing code (ret) in comrvReset
    
    commit 9b066cb9e9ec673ff2a02a3db062a182a1042b85
    Merge: fbefee6 46c26ea
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Tue Jul 28 13:29:22 2020 +0300
    
        Merge remote-tracking branch 'origin/master' into loc/b-comrv-rtos
    
    commit fbefee66960b8a27a3bffdac13c24087be189874
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Tue Jul 28 11:57:20 2020 +0300
    
        CR changes:
        > rename reset function to comrvReset
        > comrvReset now gets an enum as input
        add call to comrvReset in the baremetal demo
        add critical section protection when resetting comrv control block
    
    commit 529d475ff7ac3e78dc063e24768271d9e8e44b89
    Author: ronen.haen@wdc.com <ronen.haen@wdc.com>
    Date:   Sun Jul 26 15:55:34 2020 +0300
    
        add reset function comrvResetCacheCB()

--- WD-Firmware/comrv/comrv.c
+++ WD-Firmware/comrv/comrv.c
@@ -201,8 +201,7 @@ _Pragma("clang diagnostic ignored \"-Winline-asm\"")
       ((((u32_t)(pReturnAddress)) - uiFuncOffset - ( pComrvStackFrame->ucAlignmentToMaxGroupSize << D_COMRV_GRP_SIZE_IN_BYTES_SHIFT_AMNT)) & (D_COMRV_OVL_GROUP_SIZE_MAX-1))
 
 /* Get the index of the rightmost set bit */
-// TODO: Nati - when EH2 define is set we also need to set D_BITMANIP_EXT
-#ifndef D_BITMANIP_EXT
+#ifndef __riscv_bitmanip
    /* with the absence of RISC-V bitmanip extension we use a faster method to find the
       location of the first set bit - http://supertech.csail.mit.edu/papers/debruijn.pdf */
    #define M_COMRV_GET_SET_BIT_INDEX(uiFindFirstSet)   ucArrDeBruijnBitPos[((u32_t)(uiFindFirstSet * D_COMRV_DEBRUIJN32)) >> D_COMRV_DEBRUIJN32_SHFT_AMNT]
@@ -211,7 +210,8 @@ _Pragma("clang diagnostic ignored \"-Winline-asm\"")
    // TODO: Nati - also add macros for clz, ctz.
    #define M_COMRV_GET_SET_BIT_INDEX(uiFindFirstSet)
    #error "M_COMRV_GET_SET_BIT_INDEX missing implementation"
-#endif /* D_BITMANIP_EXT */
+#endif /* __riscv_bitmanip */
+
 /**
 * types
 */
